878e8e0977bda75dce48cf83adbfef94e12cf157
Jesus Medina <jesusmedinagajardo@gmail.com>
2025-01-27 19:49:03 -0300

    feat(home): implementamos diseño y lógica para la vista completa del home y tambien añadimos logica con javascript, patrones de diseño con javascript y css, creamos estructura de carpetas y añadimos mas assets

---
+++ landing-page/js/config/config.js
@@ -0,0 +1,13 @@
+export const config = {
+    menuItems: document.querySelectorAll(".navbar__menu-item a"),
+    whatsappContainer: document.querySelector(".whatsapp-container"),
+    footer: document.querySelector(".footer"),
+    copyleftContainer: document.querySelector(".copyleft__container"),
+    scrollContainer: document.querySelector(".scroll-container"),
+    sections: document.querySelectorAll("section"),
+    home: document.getElementById("home"),
+    navbar: document.querySelector(".navbar"),
+    facebookIcon: document.getElementById("facebook-icon"),
+    instagramIcon: document.getElementById("instagram-icon"),
+    botonUp: document.getElementById("boton-up"),
+};
